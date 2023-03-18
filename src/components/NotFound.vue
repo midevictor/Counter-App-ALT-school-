6a54f5272e3297cc5dc7a6e53527a2af303e3d32
Mide Victor <midevictor90@gmail.com>
2023-03-18 10:14:27 +0100

    ready to be deployed

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,8 +3,10 @@
     <div class='image_wrapper'>
       <img src='@/assets/404.svg' />
     </div>
+    <h1 class=" head">Oops !</h1>
     <h1 class='heading-main'>404 Not Found</h1>
     <p class='error__msg'>Sorry, the page you requested could not be found.</p>
+    <button type='button' class='btn'><router-link to="/">Back To Home</router-link> </button>
   </main>
 </template>
 
@@ -26,12 +28,18 @@ export default {
   width: 98%;
   margin: 0 auto;
 }
+
 .image_wrapper{
   width: 200px;
   height: 200px;
 }
 .image_wrapper img{
   width: 100%;
+}
+.head{
+  font-size: 6rem/* 96px */;
+  line-height: 1;
+  font-weight: 700;
 }
 .heading-main{
   font-size: 48px;
@@ -45,4 +53,19 @@ export default {
   font-size: 16px;
   text-align: center;
 }
+.btn{
+  height: 4rem/* 64px */;
+  padding-left: 1.5rem/* 24px */;
+  padding-right: 1.5rem/* 24px */;
+  min-height: 4rem/* 64px */;
+  font-size: 1.125rem/* 18px */;
+  background-color: rgb(17, 24, 39, 1);
+  color: #fff;
+  border-radius: 0.5rem/* 4px */;
+}
+.btn a{
+  color: #fff;
+  text-decoration: none;
+}
+
 </style>
